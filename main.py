e4b1fcfb3d21853697c91d9d3dd90709ff13beb7
Fineboy94449 <kobdry@gmail.com>
2025-09-26 15:46:03 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# CAL94449
+print('cal94449'.upper())
+
+print()
+print('FINANCIAL CALCULATOR')
+
+print()
+print('SIMPLE_INTEREST CAL')
+
+# Choose Your carrency
+
+fiat = {
+  'ZAR',
+  'USD', 
+  'AUD',
+  'NGN'
+ }
+
+ 
+crypto = {
+  'BTC',
+  'ETH',
+  'BNB',
+  'XRP',
+  'SUI'
+ }
+
+crypto_stable_coin = {
+    'USDC',
+    'USDT',
+    'AUSD',
+    'EURC',
+    'USD1',
+}
+
+# select your carrency
+
+Carrrency = input('Select your carrency :')
+
+
